bced2d28fad2480c37a7de218d0c512c0381cc90
yasinkuyuk <yasinkuyuk@gmail.com>
2021-07-22 22:53:07 +0300

    randomPlay method added. but not works correctly

--- venv/classes.py
+++ venv/classes.py
@@ -7,7 +7,7 @@ class Card:
         self.number = nm
 
     def __gt__(self,second):
-        if self.number == 0 && second.number != 0:
+        if self.number == 0 and second.number != 0:
             return True
         else:
             if self.number > second.number:
@@ -16,7 +16,7 @@ class Card:
                 return False
     
     def __lt__(self,second):
-        if second.number == 0 && self.number != 0:
+        if second.number == 0 and self.number != 0:
             return True
         else:
             if self.number < second.number:
@@ -24,7 +24,7 @@ class Card:
             else:
                 return False
         
-    def __eq__(init,second):
+    def __eq__(self,second):
         if self.number == second.number:
             return True
         return False
@@ -43,6 +43,7 @@ class Round:
         self.roundType = 0
         self.winningCard = Card()
         self.cardList = []
+
     
     def playRound(self,card,isWon,playerID):
         self.moveCount += 1
@@ -99,20 +100,20 @@ class Game:
         self.maxRound = assertion
         self.bossCard = boss
 
-    def removeCardFromList(self,playedCard):
-        for card in self.playerList[0].cards:
-            if card.colour == playedCard.colour && card.number == playedCard.number:
-                self.playerList[0].cards.remove(card)
+    def removeCardFromList(self,playedCard,playerNumber):
+        for card in self.playerList[playerNumber].cards:
+            if card.colour == playedCard.colour and card.number == playedCard.number:
+                self.playerList[playerNumber].cards.remove(card)
         return
     
     def playFirstRound(self,playedCard):
         self.round.roundType = playedCard.colour
         self.round.playRound(playedCard,True,0)
-        self.removeCardFromList(playedCard)
+        self.removeCardFromList(playedCard,0)
         return
 
-    def isColorInPlayersCards(self,colour):
-        for card in self.playerList[0].cards:
+    def isColorInPlayersCards(self,colour,playerID):
+        for card in self.playerList[playerID].cards:
             if card.colour == colour:
                 return True
         return False
@@ -123,75 +124,87 @@ class Game:
             return True
         
         if playedCard.colour != self.round.roundType:
-            if self.isColorInPlayersCards(playedCard.colour):
+            if self.isColorInPlayersCards(playedCard.colour,0):
                 print("You played wrong card. Please play the card with the same type!")
                 return False
             else:
                 if playedCard.colour != self.bossCard:
-                    if self.isColorInPlayersCards(self.bossCard):
+                    if self.isColorInPlayersCards(self.bossCard,0):
                         print("You played wrong card. Please play the card with boss type!")
                         return False
                     else:
                         self.round.playRound(playedCard,False,0)
-                        self.removeCardFromList(playedCard)
+                        self.removeCardFromList(playedCard,0)
                         if self.round.moveCount == 4:
                             self.playerList[self.round.currentWinner].roundsEarned += 1
                             winner = self.round.currentWinner
                             self.round = Round(winner)
+                            if winner != 0:
+                                self.randomPlay(winner,True)
                         return True
                 else:
                     if self.round.winningCard.colour == self.bossCard:
                         if playedCard > self.round.winningCard:
                             self.round.playRound(playedCard,True,0)
-                            self.removeCardFromList(playedCard)
+                            self.removeCardFromList(playedCard,0)
                             if self.round.moveCount == 4:
                                 self.playerList[self.round.currentWinner].roundsEarned += 1
                                 winner = self.round.currentWinner
                                 self.round = Round(winner)
+                                if winner != 0:
+                                    self.randomPlay(winner,True)
                             return True
                         else:
                             for card in self.playerList[0].cards:
-                                if card.colour == self.bossCard && card > self.round.winningCard:
+                                if card.colour == self.bossCard and card > self.round.winningCard:
                                     print("You played the wrong card! Please increase the current winning card")
                                     return False
 
                             self.round.playRound(playedCard,False,0)
-                            self.removeCardFromList(playedCard)
+                            self.removeCardFromList(playedCard,0)
                             if self.round.moveCount == 4:
                                 self.playerList[self.round.currentWinner].roundsEarned += 1
                                 winner = self.round.currentWinner
                                 self.round = Round(winner)
+                                if winner != 0:
+                                    self.randomPlay(winner,True)
                             return True
                             
                         
                     else:
                         self.round.playRound(playedCard,True,0)
-                        self.removeCardFromList(playedCard)
+                        self.removeCardFromList(playedCard,0)
                         if self.round.moveCount == 4:
                             self.playerList[self.round.currentWinner].roundsEarned += 1
                             winner = self.round.currentWinner
                             self.round = Round(winner)
+                            if winner != 0:
+                                self.randomPlay(winner,True)
                         return True
         else:
             if playedCard > self.round.winningCard:
                 self.round.playRound(playedCard,True,0)
-                self.removeCardFromList(playedCard)
+                self.removeCardFromList(playedCard,0)
                 if self.round.moveCount == 4:
                     self.playerList[self.round.currentWinner].roundsEarned += 1
                     winner = self.round.currentWinner
                     self.round = Round(winner)
+                    if winner != 0:
+                        self.randomPlay(winner,True)
                 return True
             else:
                 for card in self.playerList[0].cards:
-                    if card.colour == self.round.roundType && card > self.round.winningCard:
+                    if card.colour == self.round.roundType and card > self.round.winningCard:
                         print("You played the wrong card! Please increase the current winning card")
                         return False
                 self.round.playRound(playedCard,False,0)
-                self.removeCardFromList(playedCard)
+                self.removeCardFromList(playedCard,0)
                 if self.round.moveCount == 4:
                     self.playerList[self.round.currentWinner].roundsEarned += 1
                     winner = self.round.currentWinner
                     self.round = Round(winner)
+                    if winner != 0:
+                        self.randomPlay(winner,True)
                 return True
 
     def endGame(self):
@@ -201,18 +214,106 @@ class Game:
             print("Congratulations! You won the game. You had to win %d rounds, and you won %d\n" % (self.maxRound,self.playerList[0].roundsEarned))
 
         for i in range(1,4):
-                print("%s won %d rounds\n" % (self.playerList[i].name,self.playerList[i].roundsEarned))
+            print("%s won %d rounds\n" % (self.playerList[i].name,self.playerList[i].roundsEarned))
         return
-
-
-
-
-
-
-
-
-
     
-        
+    def randomPlay(self,playerNumber,flag):
+        if flag:
+            index = random.randint(0, len(self.playerList[playerNumber].cards)-1)
+            card = self.playerList[playerNumber].cards[index]
+            self.round.playRound(card, False, playerNumber)
+            self.removeCardFromList(card, playerNumber)
+            self.randomPlay(playerNumber+1,False)
+            return
+        else:
+            card = Card(self.round.roundType,-1)
+            isInList = False
+            for iterCard in self.playerList[playerNumber].cards:
+                if iterCard.colour == self.round.roundType:
+                    if iterCard > card:
+                        card = iterCard
+                        isInList = True
+                    
+            if isInList:
+                isWon = True
+                if self.round.winningCard.colour == self.bossCard:
+                    isWon = False
+                else:
+                    if self.round.winningCard > card:
+                        isWon = False
+                        card = Card(self.round.roundType,0)
+                        for iterCard in self.playerList[playerNumber].cards:
+                            if iterCard.colour == self.round.roundType:
+                                if card > iterCard:
+                                    card = iterCard
+                
+                self.round.playRound(card, isWon, playerNumber)
+                self.removeCardFromList(card, playerNumber)
+                if self.round.moveCount == 4:
+                    self.playerList[self.round.currentWinner].roundsEarned += 1
+                    winner = self.round.currentWinner
+                    self.round = Round(winner)
+                    if winner != 0:
+                        self.randomPlay(winner,True)
+                else:
+                    if playerNumber != 3:
+                        self.randomPlay(playerNumber+1,False)
+                return
+            else:
+                if self.isColorInPlayersCards(self.bossCard,playerNumber):
+                    isWon = True
+                    card = Card(self.bossCard,0)
+                    for iterCard in self.playerList[playerNumber].cards:
+                        if iterCard.colour == card.colour:
+                            if iterCard < card:
+                                card = iterCard
 
+                    if self.round.winningCard.colour == self.bossCard:
+                        isWon = False
+                        if card < self.round.winningCard:
+                            for iterCard in self.playerList[playerNumber].cards:
+                                if iterCard.colour == card.colour:
+                                    if iterCard > self.round.winningCard:
+                                        card = iterCard
+                                        isWon = True
+                                        break
+                    self.round.playRound(card, isWon, playerNumber)
+                    self.removeCardFromList(card, playerNumber)
+                    if self.round.moveCount == 4:
+                        self.playerList[self.round.currentWinner].roundsEarned += 1
+                        winner = self.round.currentWinner
+                        self.round = Round(winner)
+                        if winner != 0:
+                            self.randomPlay(winner,True)
+                    else:
+                        if playerNumber != 3:
+                            self.randomPlay(playerNumber+1,False)
+                    return   
+                else:
+                    index = random.randint(0, len(self.playerList[playerNumber].cards)-1)
+                    card = self.playerList[playerNumber].cards[index]
+                    self.round.playRound(card, False, playerNumber)
+                    self.removeCardFromList(card, playerNumber)
+                    if self.round.moveCount == 4:
+                        self.playerList[self.round.currentWinner].roundsEarned += 1
+                        winner = self.round.currentWinner
+                        self.round = Round(winner)
+                        if winner != 0:
+                            self.randomPlay(winner,True)
+                        return
+                    else:
+                        if playerNumber != 3:
+                            self.randomPlay(playerNumber+1,False)
+                    return 
+    
+    def printRound(self,moveCount):
+        if moveCount != 0:
+            printCards(self.round.cardList)
 
+        cardType = int(input("""Select the type of playing card: 
+        0: Spades
+        1: Hearths
+        2: Diamonds
+        3: Clovers: """))
+        cardNumber = convertCardNumberToInteger(input("Type the number or name(J,Q,K,A) playing card: "))
+        return cardType,cardNumber
--- venv/main.py
+++ venv/main.py
@@ -15,6 +15,7 @@ game = Game()
 game.createPlayers(namelist)
 game.createCardList()
 game.distributeCards()
+printCards(game.playerList[0].cards)
 
 assertion = int(input("How many game you can earn"))
 boss = int(input("""Select the type of the boss card: 
@@ -25,18 +26,14 @@ boss = int(input("""Select the type of the boss card:
 
 game.setGameRules(assertion,boss)
 
-while currentRoundCount < 13:
-    cardType = int(input("""Select the type of playing card: 
-    0: Spades
-    1: Hearths
-    2: Diamonds
-    3: Clovers"""))
-    cardNumber = convertCardNumberToInteger(input("Type the number or name(J,Q,K,A) playing card: "))
-    card = Card(cardType,cardNumber)
+while currentRoundCount < roundCount:
+    cardProperties = game.printRound(game.round.moveCount)
+    card = Card(cardProperties[0],cardProperties[1])
     while True:
-        if game.play():
+        if game.play(card):
             break
-
+    
+    game.randomPlay(1, False)
     currentRoundCount += 1
 
 game.endGame()
